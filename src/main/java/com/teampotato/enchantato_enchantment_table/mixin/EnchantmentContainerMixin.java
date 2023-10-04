affdb990d0510d611a14874b1d5228d2645ae747
Kasualix <670067575@qq.com>
2023-10-04 14:38:26 +0800

    Update EnchantmentContainerMixin.java

--- src/main/java/com/teampotato/enchantato_enchantment_table/mixin/EnchantmentContainerMixin.java
+++ src/main/java/com/teampotato/enchantato_enchantment_table/mixin/EnchantmentContainerMixin.java
@@ -11,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
-import java.util.Random;
 
 @Mixin(EnchantmentMenu.class)
 public abstract class EnchantmentContainerMixin {
